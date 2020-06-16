ab5867727025632b3c1618bebb09116bc583688a
Mr. Anderson <44845040+call-042PE@users.noreply.github.com>
2020-06-16 17:24:56 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,13 +3,13 @@
 
 int main()
 {
-	//Crée une image virtuel du module en cours exemple Anti-Dumping.exe
+	//CrÃ©e une image virtuel du module en cours exemple Anti-Dumping.exe
 	HMODULE moduleBase = GetModuleHandleA(NULL);
 	//variable pour l'anciene protection si tu veux la remettre
 	DWORD oldProtection;
 	//Change la protection de l'image virtual du module https://docs.microsoft.com/en-us/windows/win32/api/memoryapi/nf-memoryapi-virtualprotect
-	VirtualProtect((LPVOID)moduleBase, 4096, PAGE_READWRITE, &oldProtection);
+	VirtualProtect((LPVOID)moduleBase, 1000, PAGE_READWRITE, &oldProtection);
 	//Efface les headers
-	ZeroMemory(moduleBase, 4096);
-	VirtualProtect((LPVOID)moduleBase, 4096, oldProtection, &oldProtection);
+	ZeroMemory(moduleBase, 1000);
+	VirtualProtect((LPVOID)moduleBase, 1000, oldProtection, &oldProtection);
 }
